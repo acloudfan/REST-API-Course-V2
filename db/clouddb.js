2263b1e0c886f82803b9e6b0e3d991346977453d
acloudfan <raj@acloudfan.com>
2019-10-10 09:17:55 -0400

    ok

--- db/clouddb.js
+++ db/clouddb.js
@@ -2,10 +2,11 @@
  * Setup the Database URL
  */
 
+
 const DB_USER = "admin"
 const DB_PASSWORD = "admin123"
 const DB_NAME = "acmetravel"
-const CLUSTER_HOST = "cluster0-46e5h.mongodb.net"
+const CLUSTER_HOST = "PASTE CLUSTER ADDRESS HERE"
 
 // Setup the DB URI
 exports.DB_URI= "mongodb+srv://"+DB_USER+":"+DB_PASSWORD+"@"+CLUSTER_HOST+"/"+DB_NAME+"?retryWrites=true&w=majority"
