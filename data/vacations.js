bfb6005ccedd60f7a45787fffc9debefdd8efd10
acloudfan <raj@acloudfan.com>
2020-05-03 07:26:31 -0400

    Updated the validTill & Expires to 2025

--- data/vacations.js
+++ data/vacations.js
@@ -24,9 +24,9 @@ exports.SingleRow = {
     "offer": {
         "discount": 0.1,
         "description": "End of year sale",
-        "expires": "1/31/2020"
+        "expires": "1/31/2025"
     },
-    "validTill": "1/31/2020",
+    "validTill": "1/31/2025",
     "soldout": false
 }
 
@@ -56,9 +56,9 @@ exports.MultipleRows = [
     "offer": {
         "discount": 0.25,
         "description": "End of year sale",
-        "expires": "1/31/2020"
+        "expires": "1/31/2025"
     },
-    "validTill": "1/31/2020",
+    "validTill": "1/31/2025",
     "soldout": false
 
 },
@@ -91,9 +91,9 @@ exports.MultipleRows = [
     "offer": {
         "discount": 0.25,
         "description": "End of year sale",
-        "expires": "1/31/2020"
+        "expires": "1/31/2025"
     },
-    "validTill": "1/31/2020",
+    "validTill": "1/31/2025",
     "soldout": false
 
 }]
